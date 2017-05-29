05f067239ee0e238c59b830bb3d6830c9a217d72
Will Hines <whines@gmail.com>
2017-05-29 11:18:08 -0700

    When I was using a router.php

--- core/router.php
+++ core/router.php
@@ -2,29 +2,15 @@
 // parse the URL, find controller, and go!
 
 
-//parse URL into stuff
-  $path = array();
-  if (isset($_SERVER['REQUEST_URI'])) {
-    $request_path = explode('?', $_SERVER['REQUEST_URI']);
 
-    $path['base'] = rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/');
-    $path['call_utf8'] = substr(urldecode($request_path[0]), strlen($path['base']) + 1);
-    $path['call'] = utf8_decode($path['call_utf8']);
-    if ($path['call'] == basename($_SERVER['PHP_SELF'])) {
-      $path['call'] = '';
-    }
-    $path['call_parts'] = explode('/', $path['call']);
+$params = parse_incoming_url();
 
-    $path['query_utf8'] = urldecode($request_path[1]);
-    $path['query'] = utf8_decode(urldecode($request_path[1]));
-    $vars = explode('&', $path['query']);
-    foreach ($vars as $var) {
-      $t = explode('=', $var);
-      $path['query_vars'][$t[0]] = $t[1];
-    }
-  }
-return $path;
+// wrap this all up in Flow?
+// was a controller named? does it exist?
+// was a method named? does it exist in that controller?
+// can we check a number of arguments in the method? eh, just call it. it has to be handle incorrect arguments
 
-//echo '<pre>'.print_r($path, true).'</pre>';
-
+// default
+$c = ($c ? : $c : 'Workshops');
+$m = ($m ? : $m : 'index');
 
